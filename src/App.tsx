83985e02876c39fa65f4a899413a4d3dbc9f7753
yuyu <yuyu@yeah.net>
2024-01-02 15:37:13 +0800

    update msg

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>hello world!!!</p>
+        <p>hello world react</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
